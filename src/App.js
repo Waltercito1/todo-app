cf6e7b44557e0dbbcbd1b6daf44eb21ff297834f
Walter Aab <aabwalter@yahoo.com>
2022-02-05 17:15:30 -0600

    add counter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
+import Counter from './components/counter/Counter'
 import './App.css'
 
 class App extends Component {
   render() {
-    return <div className='App'>Hello World!</div>
+    return (
+      <div className='App'>
+        <Counter />
+      </div>
+    )
   }
 }
 export default App

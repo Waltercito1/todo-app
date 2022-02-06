1843c43b05558f182d53042f224449999b2c71d6
Walter Aab <aabwalter@yahoo.com>
2022-02-05 18:09:09 -0600

    add login component to todo

--- src/components/todo/TodoApp.js
+++ src/components/todo/TodoApp.js
@@ -2,7 +2,23 @@ import React, { Component } from 'react'
 
 class TodoApp extends Component {
   render() {
-    return <div className='TodoApp'>My Todo Application</div>
+    return (
+      <div className='TodoApp'>
+        <LoginComponent />
+      </div>
+    )
+  }
+}
+
+class LoginComponent extends Component {
+  render() {
+    return (
+      <div>
+        User Name: <input type='text' name='username' />
+        Password: <input type='password' name='password' />
+        <button>Login</button>
+      </div>
+    )
   }
 }
 

8dcc63d192c38e06e3a5aeeb8760c54a6a65fc0f
Walter Aab <aabwalter@yahoo.com>
2022-02-07 21:31:43 -0600

    add bootstrap

--- src/components/todo/TodoApp.js
+++ src/components/todo/TodoApp.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 import withNavigation from './WithNavigation'
 import withParams from './WithParams'
 
@@ -10,6 +10,7 @@ class TodoApp extends Component {
     return (
       <div className='TodoApp'>
         <Router>
+          <HeaderComponent />
           <Routes>
             <Route path='/' element={<LoginComponentWithNavigation />} />
             <Route path='/login' element={<LoginComponentWithNavigation />} />
@@ -18,8 +19,128 @@ class TodoApp extends Component {
               element={<WelcomeComponentWithParams />}
             />
             <Route path='*' element={<ErrorComponent />} />
+            <Route path='/todos' element={<ListTodosComponent />} />
+            <Route path='/logout' component={LogoutComponent} />
           </Routes>
+          <FooterComponent />
         </Router>
+      </div>
+    )
+  }
+}
+
+class HeaderComponent extends Component {
+  render() {
+    return (
+      <header>
+        <nav className='navbar navbar-expand-md navbar-dark bg-dark'>
+          <div>
+            <a href='https://github.com/Waltercito1' className='navbar-brand'>
+              My Github
+            </a>
+          </div>
+          <ul className='navbar-nav'>
+            <li>
+              <Link className='nav-link' to='/welcome/in28minutes'>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link className='nav-link' to='/todos'>
+                Todos
+              </Link>
+            </li>
+          </ul>
+          <ul className='navbar-nav navbar-collapse justify-content-end'>
+            <li>
+              <Link className='nav-link' to='/login'>
+                Login
+              </Link>
+            </li>
+            <li>
+              <Link className='nav-link' to='/logout'>
+                Logout
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    )
+  }
+}
+
+class FooterComponent extends Component {
+  render() {
+    return (
+      <footer className='footer'>
+        <span className='text-muted'>All Rights Reserved 2022</span>
+      </footer>
+    )
+  }
+}
+
+class LogoutComponent extends Component {
+  render() {
+    return (
+      <>
+        <h1>You are logged out</h1>
+        <div className='container'>Thank You for Using Our Application.</div>
+      </>
+    )
+  }
+}
+
+class ListTodosComponent extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      todos: [
+        {
+          id: 1,
+          description: 'Learn to Dance',
+          done: false,
+          targetDate: new Date(),
+        },
+        {
+          id: 2,
+          description: 'Become an Expert at React',
+          done: false,
+          targetDate: new Date(),
+        },
+        {
+          id: 3,
+          description: 'Visit India',
+          done: false,
+          targetDate: new Date(),
+        },
+      ],
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>List Todos</h1>
+        <div class='container'>
+          <table class='table'>
+            <thead>
+              <tr>
+                <th>description</th>
+                <th>Is Completed?</th>
+                <th>Target Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.todos.map((todo) => (
+                <tr>
+                  <td>{todo.description}</td>
+                  <td>{todo.done.toString()}</td>
+                  <td>{todo.targetDate.toString()}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     )
   }

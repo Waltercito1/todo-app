ee133644b5abcb004cf6b950aec09919262060d8
Walter Aab <aabwalter@yahoo.com>
2022-03-09 19:21:00 -0600

    Add method to check if user is loggedIn

--- src/components/todo/AuthenticationService.js
+++ src/components/todo/AuthenticationService.js
@@ -3,6 +3,16 @@ class AthenticationService {
     console.log('redisterSuccessfullLogin')
     sessionStorage.setItem('authenticatedUser', username)
   }
+
+  logout() {
+    sessionStorage.removeItem('authenticatedUser')
+  }
+
+  isUserLoggedIn() {
+    let user = sessionStorage.getItem('authenticatedUser')
+    if (user === null) return false
+    return true
+  }
 }
 
 export default new AthenticationService()
